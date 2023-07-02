e5e04a5406a5bdd3f3238d11ab35e2be6ec74b4a
Pratham Saraf <prathammsaraf@gmail.com>
2023-07-03 00:21:18 +0530

    possible fix?

--- pages/news.js
+++ pages/news.js
@@ -18,7 +18,7 @@ export default function News({ posts }) {
 }
 
 export async function getStaticProps() {
-  const response = await fetch("/api/posts");
+  const response = await fetch("http://localhost:3000/api/posts");
   const data = await response.json();
   const formattedData = data.map((post) => ({
 	id: post.id,
