43505b352005a175681980ae1b5dbf34d41bfa40
Pratham Saraf <prathammsaraf@gmail.com>
2023-07-05 22:47:16 +0530

    email image path update

--- pages/api/email.js
+++ pages/api/email.js
@@ -57,32 +57,32 @@ export default async function handler(req, res) {
                 attachments: [
                     {
                         filename: 'logo.png',
-                        path: path.resolve('images/logo.png'),
+                        path: path.join(process.cwd(), 'images','logo.png'),
                         cid: 'logo.png' 
                     },
                     {
                         filename: 'panel.jpg',
-                        path: path.resolve('images/panel.jpg'),
+                        path: path.join(process.cwd(), 'images','panel.jpg'),
                         cid: 'panel.jpg' 
                     },
                     {
                         filename: 'facebook2x.png',
-                        path: path.resolve('images/facebook2x.png'),
+                        path: path.join(process.cwd(), 'images','facebook2x.png'),
                         cid: 'facebook2x.png' 
                     },
                     {
                         filename: 'twitter2x.png',
-                        path: path.resolve('images/twitter2x.png'),
+                        path: path.join(process.cwd(), 'images','twitter2x.png'),
                         cid: 'twitter2x.png' 
                     },
                     {
                         filename: 'instagram2x.png',
-                        path: path.resolve('images/instagram2x.png'),
+                        path: path.join(process.cwd(), 'images','instagram2x.png'),
                         cid: 'instagram2x.png' 
                     },
                     {
                         filename: 'website2x.png',
-                        path: path.resolve('images/website2x.png'),
+                        path: path.join(process.cwd(), 'images','website2x.png'),
                         cid: 'website2x.png' 
                     }
                 ]
